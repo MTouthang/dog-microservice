34d964f9255ff4c8928cdf7d1e910aaec54608f9
Mamang touthang <huebart6@gmail.com>
2022-03-24 21:45:26 +0530

    final research yet to be done

--- src/main/java/mang/io/dogmicroservice/DogMicroserviceApplication.java
+++ src/main/java/mang/io/dogmicroservice/DogMicroserviceApplication.java
@@ -2,9 +2,10 @@ package mang.io.dogmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class DogMicroserviceApplication {
 
 	public static void main(String[] args) {
